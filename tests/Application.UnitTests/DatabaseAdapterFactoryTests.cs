3ce9ac119fe66e237702d0c864ce28f6dc4c5f91
jedrnow <jedrzej.nowaczyk00@gmail.com>
2025-01-10 23:24:37 +0100

    Tests

---
+++ tests/Application.UnitTests/DatabaseAdapterFactoryTests.cs
@@ -0,0 +1,50 @@
+﻿using DataVision.Application.Common.Adapters;
+using DataVision.Application.Common.Factories;
+using DataVision.Domain.Enums;
+using FluentAssertions;
+using NUnit.Framework;
+
+namespace DataVision.Application.UnitTests;
+public class DatabaseAdapterFactoryTests
+{
+    [Test]
+    public void ShouldReturnSqlServerAdapter_WhenProviderIsSqlServer()
+    {
+        // Arrange
+        var provider = DatabaseProvider.SQLServer;
+        var connectionString = "Server=myServerAddress;Database=myDataBase;User Id=myUsername;Password=myPassword;";
+
+        // Act
+        var adapter = DatabaseAdapterFactory.CreateAdapter(provider, connectionString);
+
+        adapter.Should().BeOfType<SqlServerAdapter>();
+    }
+
+    [Test]
+    public void ShouldReturnMySqlAdapter_WhenProviderIsMySql()
+    {
+        // Arrange
+        var provider = DatabaseProvider.MySQL;
+        var connectionString = "Server=myServerAddress;Database=myDataBase;User Id=myUsername;Password=myPassword;";
+
+        // Act
+        var adapter = DatabaseAdapterFactory.CreateAdapter(provider, connectionString);
+
+        // Assert
+        adapter.Should().BeOfType<MySqlAdapter>();
+    }
+
+    [Test]
+    public void ShouldReturnPostgreSqlAdapter_WhenProviderIsPostgreSql()
+    {
+        // Arrange
+        var provider = DatabaseProvider.PostgreSQL;
+        var connectionString = "Server=myServerAddress;Database=myDataBase;User Id=myUsername;Password=myPassword;";
+
+        // Act
+        var adapter = DatabaseAdapterFactory.CreateAdapter(provider, connectionString);
+
+        // Assert
+        adapter.Should().BeOfType<PostgreSqlAdapter>();
+    }
+}
---
+++ tests/Application.UnitTests/PaginationValidatorTests.cs
@@ -0,0 +1,86 @@
+﻿using DataVision.Application.Common.Models;
+using DataVision.Application.Common.Validators;
+using DataVision.Domain.Constants;
+using FluentAssertions;
+using NUnit.Framework;
+
+namespace DataVision.Application.UnitTests
+{
+    public class PaginationValidatorTests
+    {
+        [Test]
+        public void ShouldFail_WhenPageNumberIsLessThanMinimum()
+        {
+            // Arrange
+            var validator = new PaginationValidator();
+            var query = new PaginatedQuery()
+            {
+                PageNumber = DefaultValues.MinPageNumber - 1,
+                PageSize = 10
+            };
+
+            // Act
+            var result = validator.Validate(query);
+            // Assert
+            result.IsValid.Should().BeFalse();
+            result.Errors.Should().Contain(x => x.PropertyName == nameof(query.PageNumber));
+        }
+
+        [Test]
+        public void ShouldFail_WhenPageNumberIsGreaterThanMaximum()
+        {
+            // Arrange
+            var validator = new PaginationValidator();
+            var query = new PaginatedQuery()
+            {
+                PageNumber = DefaultValues.MaxPageNumber + 1,
+                PageSize = 10
+            };
+
+            // Act
+            var result = validator.Validate(query);
+
+            // Assert
+            result.IsValid.Should().BeFalse();
+            result.Errors.Should().Contain(x => x.PropertyName == nameof(query.PageNumber));
+        }
+
+        [Test]
+        public void ShouldFail_WhenPageSizeIsLessThanMinimum()
+        {
+            // Arrange
+            var validator = new PaginationValidator();
+            var query = new PaginatedQuery()
+            {
+                PageNumber = 1,
+                PageSize = DefaultValues.MinPageSize - 1
+            };
+
+            // Act
+            var result = validator.Validate(query);
+
+            // Assert
+            result.IsValid.Should().BeFalse();
+            result.Errors.Should().Contain(x => x.PropertyName == nameof(query.PageSize));
+        }
+
+        [Test]
+        public void ShouldFail_WhenPageSizeIsGreaterThanMaximum()
+        {
+            // Arrange
+            var validator = new PaginationValidator();
+            var query = new PaginatedQuery()
+            {
+                PageNumber = 1,
+                PageSize = DefaultValues.MaxPageSize + 1
+            };
+
+            // Act
+            var result = validator.Validate(query);
+
+            // Assert
+            result.IsValid.Should().BeFalse();
+            result.Errors.Should().Contain(x => x.PropertyName == nameof(query.PageSize));
+        }
+    }
+}
